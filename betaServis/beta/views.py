63bd18ff874cf883adc6abee5a628a26cf9bf3d4
andrey <andreilkt@yandex.ru>
2022-12-22 19:02:46 +0700

    Добавлен фронт

--- betaServis/beta/views.py
+++ betaServis/beta/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+import requests
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from .models import Betaservis, BetaservisText
+
+# получение данных из бд
+def index(request):
+    beta = Betaservis.objects.all()
+    return render(request, "index.html", {"beta": beta} )
+
 # Create your views here.
